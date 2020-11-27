d9394bc2062f50472529a95d882eb5ad4229544b
Creeperman007 <foltinmatej@gmail.com>
2020-11-27 23:28:09 +0100

    Example config file

---
+++ api/config.example.php
@@ -0,0 +1,13 @@
+<?php
+//Rename to config.php and fill correct credentials
+//Local credentials
+$key = ""
+
+//SE credentials
+$bearer = "";
+
+//Database credentials
+$db["host"] = "";
+$db["username"] = "";
+$db["password"] = "";
+$db["table"] = "";
