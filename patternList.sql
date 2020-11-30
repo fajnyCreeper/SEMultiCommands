59c6055abccbc5a6ef4718d5581201d8e55b4120
Creeperman007 <foltinmatej@gmail.com>
2020-11-30 14:21:16 +0100

    SQL script

---
+++ patternList.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `patternList`(
+  `pattern` varchar(10) NOT NULL,
+  `message1` varchar(500) DEFAULT NULL,
+  `message2` varchar(500) DEFAULT NULL,
+  `message3` varchar(500) DEFAULT NULL,
+  `message4` varchar(500) DEFAULT NULL,
+  `message5` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`pattern`)
+)
